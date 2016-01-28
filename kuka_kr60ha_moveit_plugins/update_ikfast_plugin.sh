8e609d2c73209749fdaf5db5f8103e449e417bfb
Simon Schmeisser (isys vision) <simon.schmeisser@isys-vision.de>
2016-01-28 14:02:26 +0100

    ikfast plugin for Kuka Kr60HA
    note that J1 axis of rotation is pointing downwards
    this is not currently supported by ikfast apparently
    https://github.com/rdiankov/openrave/issues/385
    therefore j1 is flipped manually in the wrapper code
    see //WORKAROUND J1 neg
    to find all changes

---
+++ kuka_kr60ha_moveit_plugins/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_ikfast create_ikfast_moveit_plugin.py kuka_kr60ha manipulator kuka_kr60ha_moveit_plugins /home/ros/Mikado/src/kuka_experimental/kuka_kr60ha_moveit_plugins/src/kuka_kr60ha_manipulator_ikfast_solver.cpp
